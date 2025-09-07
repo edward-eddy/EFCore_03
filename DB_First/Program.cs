65dd43aa5e53263459d1ae3d769ab8b1f6bc2e07
edwar <edwardsameh99@gmail.com>
2025-09-07 22:58:01 +0300

    Vid11

---
+++ DB_First/Program.cs
@@ -0,0 +1,39 @@
+﻿using DB_First.Data.Contexts;
+using DB_First.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DB_First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using AppDbContext context = new AppDbContext();
+
+            //Category category = new Category()
+            //{
+            //    CategoryName = "Hamda",
+            //    Description = "Hamda"
+            //};
+
+            //context.Categories.Add(category);
+
+            //context.SaveChanges();
+
+            //var result = context.Categories.ToList();
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item.CategoryName);
+            //    Console.WriteLine();
+            //    foreach (var product in item.Products)
+            //    {
+            //        Console.WriteLine(product);
+            //    }
+            //}
+
+        }
+    }
+}
---
+++ DB_First_Using_EF_Core_Power_Tool/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DB_First_Using_EF_Core_Power_Tool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
